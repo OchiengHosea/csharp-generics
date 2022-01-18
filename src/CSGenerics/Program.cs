119e033b9d4183cbbddad4e99b37763650a9ad38
OchiengHosea <jrhosea8@gmail.com>
2022-01-18 06:30:30 +0300

    Generics into

---
+++ src/CSGenerics/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CSGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackDoubles();
+        }
+
+        private static void StackDoubles()
+        {
+            var stack = new SimpleStack();
+            stack.Push(1.2);
+            stack.Push(5.2);
+            stack.Push(2.2);
+
+            double sum = 0.0;
+
+            while(stack.Count > 0)
+            {
+                double item = stack.Pop();
+                Console.WriteLine($"Item: {item}");
+                sum += item;
+
+                Console.WriteLine($"Sum: {sum}");
+                Console.ReadLine();
+            }
+        }
+
+        private static void StackStrings(){
+            var stack = new SimpleStackGeneric<string>();
+            stack.Push("Simple string");
+            stack.Push("Authority");
+            stack.Push("Manager");
+        }
+    }
+}
---
+++ src/CSGenerics/SimpleStack.cs
@@ -0,0 +1,15 @@
+using System;
+namespace CSGenerics
+{
+    public class SimpleStack
+    {
+        private readonly double[] _items;
+        private int _currentIndex = -1;
+        public SimpleStack() => _items = _items = new double[10];
+
+        public int Count => _currentIndex + 1;
+        public void Push(double item) => _items[++_currentIndex] = item;
+
+        public double Pop() => _items[_currentIndex--];
+    }
+}
